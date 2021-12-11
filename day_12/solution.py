ff5dbe9e49017c005af2b09b9e8a295c8b4e69d4
Mushinako <8977737+Mushinako@users.noreply.github.com>
2021-12-10 23:20:24 -0800

    Day 12 preparation

---
+++ day_12/solution.py
@@ -0,0 +1,32 @@
+# pyright: reportMissingTypeStubs=false
+""""""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from utils import SolutionAbstract
+
+if TYPE_CHECKING:
+    _Data = type
+
+
+class Solution(SolutionAbstract):
+    day = 12
+    data: _Data
+
+    @staticmethod
+    def _process_data(raw_data: list[str]) -> _Data:
+        """
+        Process day 12 data.
+        """
+
+    def part_1(self) -> ...:
+        """
+        Day 12 part 1 solution.
+        """
+
+    def part_2(self) -> ...:
+        """
+        Day 12 part 2 solution.
+        """
